6cbc66d729d81ae85f9e90f96b82c2ad8ae13296
NickBulbeck <nickbulbeck@btinternet.com>
2017-05-07 18:01:01 +0100

    Update SQL file to pre-load months

--- Code/db/money_cashboard.sql
+++ Code/db/money_cashboard.sql
@@ -22,3 +22,64 @@ CREATE TABLE months (
   start_date DATE,
   end_date DATE
 );
+
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Jan 2017','2017-01-01','2017-01-31');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Feb 2017','2017-02-01','2017-02-28');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Mar 2017','2017-03-01','2017-03-31');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Apr 2017','2017-04-01','2017-04-30');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('May 2017','2017-05-01','2017-05-31');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Jun 2017','2017-06-01','2017-06-30');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Jul 2017','2017-07-01','2017-07-31');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Aug 2017','2017-08-01','2017-08-31');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Sep 2017','2017-09-01','2017-09-30');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Oct 2017','2017-10-01','2017-10-31');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Nov 2017','2017-11-01','2017-11-30');
+INSERT INTO months
+  (name,start_date,end_date)
+VALUES
+  ('Dec 2017','2017-12-01','2017-12-31');
+
+
+
+
+
+
+
+
+
+
+
+
