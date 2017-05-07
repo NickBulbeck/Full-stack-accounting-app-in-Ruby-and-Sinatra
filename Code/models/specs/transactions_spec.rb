98b0959ebee7c092208b0973384025665d5050ca
NickBulbeck <nickbulbeck@btinternet.com>
2017-05-07 17:40:50 +0100

    Add transactions model / TDD spec

--- Code/models/specs/transactions_spec.rb
+++ Code/models/specs/transactions_spec.rb
@@ -6,17 +6,45 @@ require_relative('../transactions.rb')
 class TestTransactions < MiniTest::Test
 
   def setup
-    # @transaction1 = Transaction.new()
+    @test_transaction = Transaction.new({
+      'transaction_date' => '2017-05-01',
+      'payee' => 'Co-Op',
+      'amount' => 5.41,
+      'category_id' => 1 
+    })
   end
 
   def test_can_create_transaction
     test_transaction = Transaction.new({
       'transaction_date' => '2017-05-01',
-      'payee' => 'Co_op',
+      'payee' => 'Co-Op',
       'amount' => 5.41,
       'category_id' => 1 
     })
+  end
 
+  def test_can_read_transaction_date
+    result = @test_transaction.transaction_date
+    assert_equal("2017-05-01",result)
+  end
+
+  def test_can_read_payee
+    result = @test_transaction.payee
+    assert_equal("Co-Op",result)
+  end
+
+  def test_can_read_amount
+    result = @test_transaction.amount
+    assert_equal(5.41,result)
+  end
+
+  def test_can_read_category_id
+    result = @test_transaction.category_id
+    assert_equal(1,result)
   end
 
 end
+
+
+
+
--- Code/models/transactions.rb
+++ Code/models/transactions.rb
@@ -3,9 +3,11 @@ require_relative('../db/sql_runner.rb')
 
 class Transaction
 
+  attr_reader :id, :transaction_date, :payee, :amount, :category_id
+
   def initialize(params)
     @id = params['id'].to_i if params['id']
-    @transacion_date = params['transaction_date']
+    @transaction_date = params['transaction_date']
     @payee = params['payee']
     @amount = params['amount'].to_f
     @category_id = params['category_id'].to_i
