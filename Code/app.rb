871d9690b5e455c9385fe6284b7b0fb277e9b5b9
NickBulbeck <nickbulbeck@btinternet.com>
2017-05-10 16:24:20 +0100

    Complete styling to Version 1

--- Code/app.rb
+++ Code/app.rb
@@ -9,5 +9,18 @@ get '/' do
   @current_month = Date.today.month
   @current_year = Date.today.year
   @current_day = Date.today.day
-  erb( :index )
+
+ @total_spend = Transaction.total_spend
+    @total_budget = Category.total_budget
+    @prorata_budget = @total_budget / 12
+    @current_month = Date.today.month.to_i
+    @budget_to_date = @prorata_budget * @current_month
+
+    if @total_spend < @budget_to_date
+      @report_text = "You're within budget! There's a mountain view here to celebrate..."
+    else
+    @report_text = "You're not within budget. To show a nice background image... cut back on your spending!"
+      end
+ erb( :index )
+
 end
